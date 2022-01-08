8a25a6df7a68bbf6db3032b3a8563d53c9f47733
Bradford Gill <bradford42400@gmail.com>
2022-01-07 22:16:51 -0500

    add history logging

--- train.py
+++ train.py
@@ -8,18 +8,21 @@ CLASSES = 200
 
 BATCH_SIZE = 64
 TARGET_SIZE = (256, 256)
-EPOCHS = 64
+EPOCHS = 20
 
 # optimizer 
 MOMENTUM = .9
 INIT_LR = 1e-6
 OPT = tf.keras.optimizers.SGD(INIT_LR, momentum = MOMENTUM)
 
-LR_CB = tf.keras.callbacks.ReduceLROnPlateau('val_loss', factor = .3, patience = 2, verbose = 1, min_lr = INIT_LR/100)
+LR_CB = tf.keras.callbacks.ReduceLROnPlateau('val_loss', factor = .3, patience = 4, verbose = 1, min_lr = INIT_LR/100)
 
 # loss
-LABEL_SMOOTHING = 0.0
+LABEL_SMOOTHING = 0.1
 LOSS = tf.keras.losses.CategoricalCrossentropy(label_smoothing = LABEL_SMOOTHING)
+WEIGHT_DECAY = 1e-5
+
+DROPOUT_RATE = .5
 
 METRICS = [
     tf.keras.metrics.CategoricalAccuracy(),
@@ -29,12 +32,18 @@ METRICS = [
 
 CALLBACKS = [LR_CB]
 
+
 def main():
     base_model = tf.keras.applications.Xception(classes = CLASSES, weights = "imagenet", include_top = False)
+
+    for layer in base_model.layers:
+        if isinstance(layer, (tf.keras.layers.Conv2D, tf.keras.layers.SeparableConv2D)): # check if it a conv layer and add dropout
+            layer.kernel_regularizer = tf.keras.regularizers.l2(WEIGHT_DECAY)
 
     inputs = tf.keras.Input(shape=(*TARGET_SIZE, 3))
     x = base_model(inputs, training = False)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(DROPOUT_RATE)
     outputs = tf.keras.layers.Dense(CLASSES, activation = 'softmax')(x)
     model = tf.keras.Model(inputs, outputs)
 
@@ -58,7 +67,7 @@ def main():
         metrics = METRICS
         )
 
-    runname = utils.make_runname('init')
+    runname = utils.make_runname('pretrain_freeze')
 
     history = model.fit(train_gen, 
         validation_data = val_gen,
--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
 import boto3
 from CONFIG import KEY_ID, SECRET_KEY
-import numpy as np
 import os
 import datetime
+import numpy as np
 
-def make_runname(prefix):
-    return f'{prefix}_{datetime.datetime.now(datetime.timezone.utc).strftime("%y%m%d%H%M%S")}'
+def make_runname(prefix = None):
+    if isinstance(prefix, str):
+        return f'{prefix}_{datetime.datetime.now(datetime.timezone.utc).strftime("%y%m%d%H%M%S")}'
+    else: 
+        return datetime.datetime.now(datetime.timezone.utc).strftime("%y%m%d%H%M%S")
+
 
 def save_model(runname, model, bucketname = 'bradfordgillbirddatabucket', foldername = 'modellogs'):
     filename = f'{runname}.h5'
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+
     path = os.path.join(foldername, filename)
 
     session = boto3.Session(
@@ -34,5 +41,39 @@ def load_model(runname, bucketname = 'bradfordgillbirddatabucket', foldername = 
 
     return load_model(path)
 
+def log_history(runname, history, bucketname = 'bradfordgillbirddatabucket', foldername = 'historylogs'):
+    filename = f'{runname}.npy'
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+
+    path = os.path.join(foldername, filename)
+
+    np.save(path, history.history)
+
+    session = boto3.Session(
+            aws_access_key_id=KEY_ID,
+            aws_secret_access_key=SECRET_KEY)
+
+    s3 = session.resource('s3')
+    result = s3.Bucket(bucketname).upload_file(path, path)
+
+def get_history(runname,  bucketname = 'bradfordgillbirddatabucket', foldername = 'historylogs'):
+    filename = f'{runname}.npy'
+    path = os.path.join(foldername, filename)
+
+    if not os.path.exists(path):
+        if not os.path.exists(foldername):
+            os.mkdir(foldername)
+
+        session = boto3.Session(
+                aws_access_key_id=KEY_ID,
+                aws_secret_access_key=SECRET_KEY)
+
+        s3 = session.resource('s3')
+        result = s3.Bucket(bucketname).download_file(path, path)
+
+    return np.load(path, allow_pickle = True)
+
 if __name__ == '__main__':
-    print(make_runname('test'))
+    pass
+   
