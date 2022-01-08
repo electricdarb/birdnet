7865305f0750b4dbf72a0a4997a930df22d32bbd
Bradford Gill <bradford42400@gmail.com>
2022-01-07 23:29:40 -0500

    add

--- downloaddata.py
+++ downloaddata.py
@@ -8,10 +8,16 @@ def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
             aws_secret_access_key=SECRET_KEY)
     s3 = session.resource('s3')
     bucket = s3.Bucket(bucketName) 
+    print('Starting!')
+    i = 0
     for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
         if not os.path.exists(os.path.dirname(obj.key)):
             os.makedirs(os.path.dirname(obj.key))
         bucket.download_file(obj.key, obj.key)
+        if i % 100 == 0:
+            print(i/11500)
+        i += 1
+    
 
 if __name__ == "__main__":
     downloadDirectoryFroms3('bradfordgillbirddatabucket', 'cub200data')
--- train.py
+++ train.py
@@ -15,12 +15,16 @@ MOMENTUM = .9
 INIT_LR = 0.005
 OPT = tf.keras.optimizers.SGD(INIT_LR, momentum = MOMENTUM)
 
+def scheduler(epoch, lr):
+    return 1e-7 * 10 ** (7 * epoch/EPOCHS)
+
+#LR_CB = tf.keras.callbacks.LearningRateScheduler(scheduler)
 LR_CB = tf.keras.callbacks.ReduceLROnPlateau('val_loss', factor = .3, patience = 4, verbose = 1, min_lr = INIT_LR/100)
 
 # loss
 LABEL_SMOOTHING = 0.1
 LOSS = tf.keras.losses.CategoricalCrossentropy(label_smoothing = LABEL_SMOOTHING)
-WEIGHT_DECAY = 1e-5
+WEIGHT_DECAY = 1e-5 
 
 DROPOUT_RATE = .5
 
@@ -32,20 +36,22 @@ METRICS = [
 
 CALLBACKS = [LR_CB]
 
-
 def main():
     base_model = tf.keras.applications.Xception(classes = CLASSES, weights = "imagenet", include_top = False)
-
-    for layer in base_model.layers:
-        if isinstance(layer, (tf.keras.layers.Conv2D, tf.keras.layers.SeparableConv2D)): # check if it a conv layer and add dropout
-            layer.kernel_regularizer = tf.keras.regularizers.l2(WEIGHT_DECAY)
+    base_model.trainable = False
 
     inputs = tf.keras.Input(shape=(*TARGET_SIZE, 3))
-    x = base_model(inputs, training = False)
+    x = base_model(inputs)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dropout(DROPOUT_RATE)
+    x = tf.keras.layers.Dropout(DROPOUT_RATE)(x)
     outputs = tf.keras.layers.Dense(CLASSES, activation = 'softmax')(x)
     model = tf.keras.Model(inputs, outputs)
+
+    for layer in model.layers[1].layers:
+        if isinstance(layer, (tf.keras.layers.Conv2D, tf.keras.layers.SeparableConv2D)): # check if it a conv layer and add dropout
+            layer.kernel_regularizer = tf.keras.regularizers.l2(WEIGHT_DECAY)
+            
+    model.layers[3].rate = DROPOUT_RATE
 
     train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
         rescale = 1./255,
@@ -67,15 +73,15 @@ def main():
         metrics = METRICS
         )
 
-    runname = utils.make_runname('pretrain_freeze')
+    runname = utils.make_runname('lrfind')
 
     history = model.fit(train_gen, 
         validation_data = val_gen,
         epochs = EPOCHS, 
         callbacks = CALLBACKS)
     
-    utils.log_history(history)
-    utils.save_model(runname)
+    utils.log_history(runname, history)
+    utils.save_model(runname, model)
     print(f'run name: {runname}')
 
 if __name__ == "__main__":
