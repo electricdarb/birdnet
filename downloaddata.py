0bf5e04ca25411a0fc973145a63389e30921f129
Bradford Gill <51732841+electricdarb@users.noreply.github.com>
2022-02-04 15:03:37 -0500

    add tqdm

--- downloaddata.py
+++ downloaddata.py
@@ -1,22 +1,21 @@
 import boto3
 import os
 from CONFIG import KEY_ID, SECRET_KEY
+from tqdm import tqdm
 
 def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
     session = boto3.Session(
             aws_access_key_id=KEY_ID,
             aws_secret_access_key=SECRET_KEY)
+    
     s3 = session.resource('s3')
     bucket = s3.Bucket(bucketName) 
-    print('Starting!')
-    i = 0
-    for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
+    
+    for obj in tqdm(bucket.objects.filter(Prefix = remoteDirectoryName)):
         if not os.path.exists(os.path.dirname(obj.key)):
             os.makedirs(os.path.dirname(obj.key))
         bucket.download_file(obj.key, obj.key)
-        if i % 100 == 0:
-            print(i/11500)
-        i += 1
+    
     
 
 if __name__ == "__main__":
